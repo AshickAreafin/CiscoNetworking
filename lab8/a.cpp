6e9f1e72f214bd843d59be4a6885d5e4bd978896
AshickAreafin <ashick.bau@gmail.com>
2020-02-29 12:49:51 +0600

    cicco_networking

---
+++ lab8/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a = 0, b = 0, c = 0; cin >> a >> b >> c;
+
+    for(int i = 1; i <= a; a += 1) {
+        for(int j = 1; j <= b; j += 1) {
+            for(int k = 1; k <= c; k += 1) {
+                if( ase[ {i, j, k} ] ) continue;
+
+            }
+        }
+    }
+    return 0;
+}
